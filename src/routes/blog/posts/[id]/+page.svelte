4f3fdca165ddbd77d97647a93b27520ef3ecf504
pikechannel <pikechannel7@gmail.com>
2022-10-16 23:30:29 +0900

    sveltekitに変更

---
+++ src/routes/blog/posts/[id]/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let data = [];
+
+    const title = data.post.title.rendered;
+    const contents = data.post.content.rendered;
+    const thumbnailURL = data.post.jetpack_featured_media_url;
+
+   
+</script>
+
+<svelte:head>
+	{@html data.post.yoast_head}
+</svelte:head>
+
+<main>
+    <h1>{title}</h1>
+
+    <div id="thumbnail_img_box">
+        <img id="thumbnail_img" src="{thumbnailURL}" alt="">
+    </div>
+    
+    <section>
+        {@html contents}
+    </section>
+</main>
+
+<style>
+    #thumbnail_img_box {
+        width: 100%;
+        text-align: center;
+    }
+    #thumbnail_img {
+        max-width: 80%;
+        margin: 0 auto;
+    }
+</style>
---
+++ src/routes/blog/tags/[id]/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+	export let data;
+	const posts = data.post
+	const title = import.meta.env.VITE_TITLE
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		{title}
+	</h1>
+
+	<ul>
+		{#each posts as post}
+		  <!-- <a href="{post.link}">
+			<li>{post.title.rendered}</li>
+		  </a> -->
+		  <a href="/blog/posts/{post.id}">
+			<li>{post.title.rendered}</li>
+		  </a>
+		{/each}
+	  </ul>
+	
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
