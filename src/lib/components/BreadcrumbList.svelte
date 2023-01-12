502b4f48fa57db6da83aa0526e98f5fe61f913c0
pikechannel <pikechannel7@gmail.com>
2023-01-13 00:59:54 +0900

    パンくずリストのコード作成

---
+++ src/lib/components/BreadcrumbList.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let breadcrumbList;
+</script>
+
+<nav>
+    <ol class="breadcrumb">
+        {#each breadcrumbList as breadcrumb}
+            {#if breadcrumb.path}
+                <li><a href={breadcrumb.path}>{breadcrumb.name}</a></li>
+            {/if}
+            {#if !breadcrumb.path}
+                <li>{breadcrumb.name}</li>
+            {/if}
+        {/each}
+    </ol>
+</nav>
+
+<style>
+    .breadcrumb {
+        display: flex;
+        flex-wrap: wrap;
+        list-style: none;
+    }
+
+    .breadcrumb li:not(:last-of-type)::after {
+        content: ">";
+        margin: 0 0.6em; /* 記号の左右の余白 */
+        color: #777; /* 記号の色 */
+    }
+
+    .breadcrumb a {
+        color: inherit;
+        text-decoration: none;
+    }
+</style>
