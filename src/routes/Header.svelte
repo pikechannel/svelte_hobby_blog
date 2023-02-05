2eaae5b4bb4afed799c21ad43f11a971b8d3f07f
pikechannel <pikechannel7@gmail.com>
2023-02-05 22:58:25 +0900

    子カテゴリまで表示可能なように修正

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,33 +1,73 @@
 <script>
 	import { page } from "$app/stores";
 
-	export let data;
-	console.log(data);
+	export let categories;
 
-	let categories = [];
-	const domain = import.meta.env.VITE_WP_DOMAIN;
-	fetch(`https://${domain}/wp-json/wp/v2/categories/`)
-		.then((response) => response.json())
-		.then((data) => {
-			categories = data;
-		})
-		.catch((error) => {
-			console.log(error);
-			return [];
+	let categoryMap = [];
+	let categoryHaveParent = [];
+
+	categories.forEach((c) => {
+		if (c.parent === 0) {
+			c["children"] = [];
+			categoryMap.push(c);
+		} else {
+			categoryHaveParent.push(c);
+		}
+	});
+
+	function compare(a, b) {
+		var r = 0;
+		if (a.id < b.id) {
+			r = -1;
+		} else if (a.id > b.id) {
+			r = 1;
+		}
+
+		return r;
+	}
+
+	categoryHaveParent.sort(compare);
+
+	categoryMap.forEach((cm) => {
+		let childCategories = categoryHaveParent.filter((chp) => {
+			if (cm.id == chp.parent) return true;
 		});
+		cm["children"] = childCategories;
+	});
 </script>
 
 <header>
 	<nav>
-		<ul>
-			<li class:active={$page.url.pathname === "/about"}>
-				<a href="/">ホーム</a>
+		<ul id="top_menu_list">
+			<li class:active={$page.url.pathname === "/"} class="top_menu">
+				<a href="/">HOME</a>
 			</li>
-			{#each categories as category}
-				<li class:active={$page.url.pathname === "/about"}>
+			{#each categoryMap as category}
+				<li
+					class:active={$page.url.pathname ===
+						"/blog/category/" + category.id}
+					class="top_menu"
+				>
 					<a data-sveltekit-reload href="/blog/category/{category.id}"
 						>{category.name}</a
 					>
+					{#if category.children.length > 0}
+						<ul id="sub_menu_list">
+							{#each category.children as children}
+								<li
+									class:active={$page.url.pathname ===
+										"/blog/category/" + children.id}
+									class="sub_menu"
+								>
+									<a
+										data-sveltekit-reload
+										href="/blog/category/{children.id}"
+										>{children.name}</a
+									>
+								</li>
+							{/each}
+						</ul>
+					{/if}
 				</li>
 			{/each}
 		</ul>
@@ -35,7 +75,6 @@
 </header>
 
 <style>
-
 	.corner {
 		width: 3em;
 		height: 3em;
@@ -56,24 +95,27 @@
 	}
 
 	nav {
-		/* display: flex;
-		justify-content: center; */
 		--background: rgba(255, 255, 255, 0.7);
-		overflow-x: auto;
 		border-bottom: solid 1px;
+	}
+
+	@media screen and (max-width: 640px) {
+		nav {
+			overflow-x: auto;
+		}
 	}
 
 	path {
 		fill: var(--background);
 	}
 
-	ul {
+	ul#top_menu_list {
 		display: flex;
 		width: max-content;
 		margin: 0 auto;
 	}
 
-	li {
+	ul#top_menu_list li.top_menu {
 		display: inline-block;
 		height: 50px;
 		padding: 0 25px;
@@ -81,14 +123,16 @@
 		align-items: center;
 		justify-content: center;
 		font-size: 18px;
+		position: relative;
 	}
 
-	li a {
+	ul#top_menu_list li.top_menu a {
 		text-decoration: none;
 		color: var(--color-text);
+		font-family: "Solitreo", cursive;
 	}
 
-	li.active::before {
+	ul#top_menu_list li.top_menu.active::before {
 		--size: 6px;
 		content: "";
 		width: 0;
@@ -102,5 +146,38 @@
 
 	a:hover {
 		color: var(--color-theme-1);
+	}
+
+	ul#top_menu_list li.top_menu:hover #sub_menu_list {
+		display: block !important;
+	}
+
+	ul#sub_menu_list {
+		position: absolute;
+		top: 100%;
+		left: 0;
+		z-index: 9999;
+		text-shadow: none;
+		display: none;
+		background-color: #323232;
+		padding-left: 0;
+		width: 200px;
+	}
+	ul#sub_menu_list li {
+		list-style: none;
+		overflow: visible;
+		height: 54px;
+		line-height: 54px;
+		transition: 0.2s;
+		color: white;
+		border-top: 1px solid white;
+	}
+
+	ul#sub_menu_list li a {
+		display: block;
+		padding-left: 10px;
+	}
+	ul#sub_menu_list li a:hover {
+		background: rgba(255, 255, 255, 0.15);
 	}
 </style>
