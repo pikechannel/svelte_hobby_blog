e48d1c60c4e72aa787e9ce6090e8f4778db30e27
pikechannel <pikechannel7@gmail.com>
2023-02-19 02:12:02 +0900

    Twitterの埋め込み用コンポーネント

---
+++ src/lib/components/Twitter.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { onMount } from 'svelte';
+
+    const twitterID = import.meta.env.VITE_TWITTER_ID;
+    
+    onMount(() => {
+        let tweetScript = document.createElement('script')
+        tweetScript.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+        document.head.appendChild(tweetScript)
+    })
+    
+</script>
+
+<a class="twitter-timeline" href="https://twitter.com/{twitterID}?ref_src=twsrc%5Etfw" data-width="300">Tweets by pike45587221</a>
