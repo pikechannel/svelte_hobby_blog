e35ebf73fc526265ea954a8f4a49250854eddd25
pikechannel <pikechannel7@gmail.com>
2023-02-19 02:13:11 +0900

    検索が実施可能なように修正

--- src/routes/blog/search/+page.svelte
+++ src/routes/blog/search/+page.svelte
@@ -1,6 +1,18 @@
-<script async src="https://cse.google.com/cse.js?cx=c55c61363cb914827">
-    const title = import.meta.env.VITE_TITLE;
+<script>
+    import { onMount } from "svelte";
+
+    const id = import.meta.env.VITE_GOOGLE_SEARCH_ID;
+
+    onMount(() => {
+        let googleSearchScript = document.createElement("script");
+        googleSearchScript.setAttribute(
+            "src",
+            `https://cse.google.com/cse.js?cx=${id}`
+        );
+        document.head.appendChild(googleSearchScript);
+    });
 </script>
+
 <div class="gcse-search" />
 
 <style></style>
