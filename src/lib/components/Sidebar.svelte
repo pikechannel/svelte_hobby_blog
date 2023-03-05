63825a94472c846814de9cf9e7f0907df0e0f9e1
pikechannel <pikechannel7@gmail.com>
2023-03-05 15:44:25 +0900

    サイドバー用のコンポーネント

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Twitter from "$lib/components/Twitter.svelte";
+    import BlogVillage from "./BlogVillage.svelte";
+</script>
+
+<section>
+    <BlogVillage />
+    <Twitter />
+</section>
