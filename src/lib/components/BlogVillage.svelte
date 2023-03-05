0b2778a90918eb26d5f4536b6b20ef29b72bc51e
pikechannel <pikechannel7@gmail.com>
2023-03-05 15:43:58 +0900

    ブログ村のランキング表示用コンポーネント

---
+++ src/lib/components/BlogVillage.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { onMount } from "svelte";
+
+    const dataChid = import.meta.env.VITE_BLOGVILLAGE_DATA_CHID;
+    const category = import.meta.env.VITE_BLOGVILLAGE_DATA_CATEGORY;
+    onMount(() => {
+        let blogVillageScript = document.createElement("script");
+        blogVillageScript.setAttribute(
+            "src",
+            "https://blogparts.blogmura.com/js/parts_view.js"
+        );
+        document.head.appendChild(blogVillageScript);
+    });
+</script>
+
+<div
+    class="blogmura-blogparts"
+    data-chid={dataChid}
+    data-category={category}
+    data-type="pv"
+/>
